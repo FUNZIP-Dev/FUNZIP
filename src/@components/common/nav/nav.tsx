49f1b0e3c05ddb7bc435cff375dbf5f69a5f3a71
Seojisoo20191941 <amy0480@naver.com>
2023-06-06 19:56:00 +0900

    :sparkles: Feat : 네브바 라우팅 구현
    feat : 네브바 라우팅 구현

--- src/@components/common/nav/nav.tsx
+++ src/@components/common/nav/nav.tsx
@@ -1,15 +1,22 @@
+import { useNavigate } from "react-router-dom";
 import { NavLogoIc } from "../../../assets";
 import * as S from "./style";
 
 export default function Nav() {
+  const navigate = useNavigate();
+
+  const handleMoveToPage = (route: string) => {
+    navigate(route);
+  };
+
   return (
     <S.NavBar>
       <NavLogoIc />
-      <S.Text>서비스 소개</S.Text>
-      <S.Text>주문하기</S.Text>
-      <S.Text>튜토리얼</S.Text>
-      <S.Text>FAQ</S.Text>
-      <S.Text>후기</S.Text>
+      <S.Text onClick={() => handleMoveToPage("/")}>서비스 소개</S.Text>
+      <S.Text onClick={() => handleMoveToPage("/order")}>주문하기</S.Text>
+      <S.Text onClick={() => handleMoveToPage("/tutorial")}>튜토리얼</S.Text>
+      <S.Text onClick={() => handleMoveToPage("/faq")}>FAQ</S.Text>
+      <S.Text onClick={() => handleMoveToPage("/review")}>후기</S.Text>
       <S.ProfileImageWrapper>
         <S.ProfileImage
           src="https://item.kakaocdn.net/do/dbcd799aac800f6a680f1d2b04cee5688f324a0b9c48f77dbce3a43bd11ce785"
