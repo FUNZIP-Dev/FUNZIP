46cb70a049bfb26a4998498340ff4cc3882c5913
Seojisoo20191941 <amy0480@naver.com>
2023-06-06 20:51:34 +0900

    :sparkles: Feat : 주문 프로그레스 구현
    feat : 주문 프로그레스 구현

--- src/@components/order/orderHeader/orderHeader.tsx
+++ src/@components/order/orderHeader/orderHeader.tsx
@@ -1,7 +1,18 @@
+import { ORDER_PROGRESS } from "../../../core/order/orderStep";
 import { StepProp } from "../../../type/order/stepProps";
 
 export default function OrderHeader(props: StepProp) {
   const { step } = props;
 
-  return <div>orderHeader</div>;
+  return (
+    <>
+      {ORDER_PROGRESS.map(({ id, text }) => (
+        <StepWrapper key={id}>
+          <Rectangle />
+          <Text>{text}</Text>
+        </StepWrapper>
+      ))}
+      <hr />
+    </>
+  );
 }
--- src/core/order/orderStep.ts
+++ src/core/order/orderStep.ts
@@ -5,3 +5,15 @@ export const ORDER_STEP = {
   OPTION: "order option",
   BUY: "order buy",
 };
+
+interface OrderType {
+  id: number;
+  text: string;
+}
+
+export const ORDER_PROGRESS: OrderType[] = [
+  { id: 1, text: "카테고리 선택" },
+  { id: 2, text: "스타일 선택" },
+  { id: 3, text: "옵션 추가" },
+  { id: 4, text: "주문하기" },
+];
