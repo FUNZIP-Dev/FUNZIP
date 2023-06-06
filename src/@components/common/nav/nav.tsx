a49ca3ff9940ca4cf6123192b353bd18a2cd4891
Seojisoo20191941 <amy0480@naver.com>
2023-06-06 19:50:08 +0900

    :lipstick: Design : 네브바 스타일링
    design : 네브바 스타일링

--- src/@components/common/nav/nav.tsx
+++ src/@components/common/nav/nav.tsx
@@ -5,17 +5,17 @@ export default function Nav() {
   return (
     <S.NavBar>
       <NavLogoIc />
-      <p>서비스 소개</p>
-      <p>주문하기</p>
-      <p>튜토리얼</p>
-      <p>FAQ</p>
-      <p>후기</p>
-      <div>
-        <img
+      <S.Text>서비스 소개</S.Text>
+      <S.Text>주문하기</S.Text>
+      <S.Text>튜토리얼</S.Text>
+      <S.Text>FAQ</S.Text>
+      <S.Text>후기</S.Text>
+      <S.ProfileImageWrapper>
+        <S.ProfileImage
           src="https://item.kakaocdn.net/do/dbcd799aac800f6a680f1d2b04cee5688f324a0b9c48f77dbce3a43bd11ce785"
           alt="프로필 이미지"
         />
-      </div>
+      </S.ProfileImageWrapper>
     </S.NavBar>
   );
 }
--- src/@components/common/nav/style.tsx
+++ src/@components/common/nav/style.tsx
@@ -2,13 +2,42 @@ import { styled } from "styled-components";
 
 export const NavBar = styled.nav`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
 
   width: 1432px;
   height: 95px;
 
   padding: 0 150px 0 110px;
+  margin-top: 30px;
+
   border-radius: 48px;
   background-color: ${({ theme }) => theme.colors.black};
+`;
+
+export const Text = styled.p`
+  color: ${({ theme }) => theme.colors.white};
+  ${({ theme }) => theme.fonts.nav}
+`;
+
+export const ProfileImage = styled.img`
+  width: 100%;
+  height: 100%;
+
+  transform: translate(50, 50);
+  object-fit: cover;
+  margin: auto;
+`;
+
+export const ProfileImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 44px;
+  height: 44px;
+
+  border-radius: 50%;
+
+  overflow: hidden;
 `;
