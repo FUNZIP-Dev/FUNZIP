848d5e69ad319fd179f42ef0b30fb2be18dbf9e6
Seojisoo20191941 <amy0480@naver.com>
2023-06-06 23:45:15 +0900

    :sparkles: Feat : 주문 메인 페이지 스텝별로 구분
    feat : 주문 메인 페이지 스텝별로 구분

--- src/@components/order/orderMain/orderMain.tsx
+++ src/@components/order/orderMain/orderMain.tsx
@@ -1,12 +1,24 @@
+import { ORDER_STEP } from "../../../core/order/orderStep";
 import { StepProp } from "../../../type/order/stepProps";
-import Tutorial from "../../common/tutorial/tutorial";
+import OrderBuy from "../orderBuy/orderBuy";
+import OrderCategory from "../orderCategory/orderCategory";
+import OrderOption from "../orderOption/orderOption";
+import OrderStyle from "../orderStyle/orderStyle";
+import OrderTutorial from "../orderTutorial/orderTutorial";
 
 export default function OrderMain(props: StepProp) {
   const { step } = props;
 
-  return (
-    <>
-      <Tutorial />
-    </>
-  );
+  switch (step) {
+    case ORDER_STEP.CATEGORY:
+      return <OrderCategory />;
+    case ORDER_STEP.OPTION:
+      return <OrderOption />;
+    case ORDER_STEP.STYLE:
+      return <OrderStyle />;
+    case ORDER_STEP.BUY:
+      return <OrderBuy />;
+    default:
+      return <OrderTutorial />;
+  }
 }
