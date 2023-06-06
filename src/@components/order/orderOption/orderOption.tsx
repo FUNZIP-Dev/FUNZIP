78e751b1b557e929439cfcb21fa82ce1cf7ef04e
Seojisoo20191941 <amy0480@naver.com>
2023-06-07 02:07:35 +0900

    :sparkles: Feat : 옵션 체크 박스 전부 구현
    feat : 옵션 체크 박스 전부 구현

--- src/@components/order/orderOption/orderOption.tsx
+++ src/@components/order/orderOption/orderOption.tsx
@@ -1,11 +1,27 @@
 import { OrderOptionCheckIc } from "../../../assets";
-import { BASIC_OPTIONS } from "../../../core/order/optionList";
+import { BASIC_OPTIONS, CAM_OPTIONS, PLUS_OPTIONS } from "../../../core/order/optionList";
 
 export default function OrderOption() {
   return (
     <>
       <h1>기본 옵션</h1>
       {BASIC_OPTIONS.map(({ id, title, content }) => (
+        <article key={id}>
+          <OrderOptionCheckIc />
+          <p>{title}</p>
+          <p>{content}</p>
+        </article>
+      ))}
+      <h1>BGM & SFX</h1>
+      {PLUS_OPTIONS.map(({ id, title, content }) => (
+        <article key={id}>
+          <OrderOptionCheckIc />
+          <p>{title}</p>
+          <p>{content}</p>
+        </article>
+      ))}
+      <h1>원본 영상 촬영캠</h1>
+      {CAM_OPTIONS.map(({ id, title, content }) => (
         <article key={id}>
           <OrderOptionCheckIc />
           <p>{title}</p>
