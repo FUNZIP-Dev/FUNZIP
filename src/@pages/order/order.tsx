503214af3402c00f4fe1382c687a7c2f9600370d
Seojisoo20191941 <amy0480@naver.com>
2023-06-06 20:24:47 +0900

    :sparkles: Feat : 주문 스텝 리코일 추가
    feat : 주문 스텝 리코일 추가

--- src/@pages/order/order.tsx
+++ src/@pages/order/order.tsx
@@ -1,6 +1,10 @@
+import { useRecoilState } from "recoil";
 import Nav from "../../@components/common/nav/nav";
+import { stepRender } from "../../recoil/order/stepRender";
 
 export default function Order() {
+  const [step, setStep] = useRecoilState(stepRender);
+
   return (
     <>
       <Nav />
---
+++ src/recoil/order/stepRender.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const stepRender = atom({
+  key: "stepRender",
+  default: "",
+});
