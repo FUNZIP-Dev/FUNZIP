1b3737c7f0b6519bb197e2ae4605809f09b1bbc8
Seojisoo20191941 <amy0480@naver.com>
2023-06-07 02:06:35 +0900

    :sparkles: Feat : 체크 박스 구현
    feat : 체크 박스 구현

--- src/@components/order/orderOption/orderOption.tsx
+++ src/@components/order/orderOption/orderOption.tsx
@@ -1,3 +1,17 @@
+import { OrderOptionCheckIc } from "../../../assets";
+import { BASIC_OPTIONS } from "../../../core/order/optionList";
+
 export default function OrderOption() {
-  return <div>orderOption</div>;
+  return (
+    <>
+      <h1>기본 옵션</h1>
+      {BASIC_OPTIONS.map(({ id, title, content }) => (
+        <article key={id}>
+          <OrderOptionCheckIc />
+          <p>{title}</p>
+          <p>{content}</p>
+        </article>
+      ))}
+    </>
+  );
 }
--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,7 @@
 export { ReactComponent as NavLogoIc } from "./icon/navLogoIc.svg";
 export { ReactComponent as OrderNextBtnIc } from "./icon/orderNextBtnIc.svg";
+export { ReactComponent as OrderOptionCheckActiveIc } from "./icon/orderOptionCheckActiveIc.svg";
+export { ReactComponent as OrderOptionCheckIc } from "./icon/orderOptionCheckIc.svg";
 export { ReactComponent as OrderPreviousBtnIc } from "./icon/orderPreviousBtnIc.svg";
 export { ReactComponent as OrderStep1Ic } from "./icon/orderStep1Ic.svg";
 export { ReactComponent as OrderStep2Ic } from "./icon/orderStep2Ic.svg";
