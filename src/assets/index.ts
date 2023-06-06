9c2b420a6bf8d9e3cfc6414299ccf1a6d7505436
Seojisoo20191941 <amy0480@naver.com>
2023-06-06 23:25:24 +0900

    :sparkles: Feat : 튜토리얼 메시지 구현
    git status

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,3 +4,4 @@ export { ReactComponent as OrderStep2Ic } from "./icon/orderStep2Ic.svg";
 export { ReactComponent as OrderStep3Ic } from "./icon/orderStep3Ic.svg";
 export { ReactComponent as OrderStep4Ic } from "./icon/orderStep4Ic.svg";
 export { ReactComponent as OrderStepBlankIc } from "./icon/orderStepBlankIc.svg";
+export { ReactComponent as TutorialMainIc } from "./icon/tutorialMainIc.svg";
