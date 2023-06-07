d71f6ce78065dbc615e84824d359eae6b7355e5a
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-06-08 01:03:07 +0900

    :sparkles: feat : authProvider
    feat : authProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { AuthProvider } from "./provider/authProvider";
+
 
 import { QueryClient, QueryClientProvider } from "react-query";
 
@@ -9,7 +11,9 @@ const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
+      <AuthProvider>
       <App />
+    </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 );
