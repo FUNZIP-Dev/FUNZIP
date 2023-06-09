ec28ea1fe07c09f637298d036be494cbf86a3aa4
Seojisoo20191941 <amy0480@naver.com>
2023-06-10 04:14:35 +0900

    :lipstick: Design : 선택한 옵션 불러와서 보여지기
    design : 선택한 옵션 불러와서 보여지기

--- src/@components/order/selectedOption/selectedOption.tsx
+++ src/@components/order/selectedOption/selectedOption.tsx
@@ -8,20 +8,38 @@ export default function SelectedOption() {
   const selectOption = useRecoilValue<SelectOptionTypes>(selectOptions);
 
   return (
-    <>
+    <S.SelectedOptionWraper>
       <S.Title>
         <S.UserName>{USER?.name}</S.UserName>님이 선택한 옵션
       </S.Title>
-      <p>기본옵션</p>
-      {selectOption?.basic?.map((basic) => (
-        <div>{basic}</div>
-      ))}
-      <p>BGM & SFX</p>
-      {selectOption?.plus?.map((plus) => (
-        <div>{plus}</div>
-      ))}
-      <p>원본 영상 촬영캠</p>
-      {selectOption?.cam}
-    </>
+      <S.TagsWrapper>
+        <S.TitleTags>
+          <S.Tags>
+            <S.Sub>기본옵션</S.Sub>
+          </S.Tags>
+          <S.Tags>
+            <S.Sub>BGM & SFX</S.Sub>
+          </S.Tags>
+          <S.Tags>
+            <S.Sub>원본 영상 촬영캠</S.Sub>
+          </S.Tags>
+        </S.TitleTags>
+        <ul>
+          <S.Tags>
+            {selectOption?.basic?.map((basic) => (
+              <S.Tag>{basic}</S.Tag>
+            ))}
+          </S.Tags>
+          <S.Tags>
+            {selectOption?.plus?.map((plus) => (
+              <S.Tag>{plus}</S.Tag>
+            ))}
+          </S.Tags>
+          <S.Tags>
+            <S.Tag>{selectOption?.cam}</S.Tag>
+          </S.Tags>
+        </ul>
+      </S.TagsWrapper>
+    </S.SelectedOptionWraper>
   );
 }
