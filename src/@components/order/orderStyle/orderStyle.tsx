356a0339d919c6d11ad5454353e565f168ba2762
Seojisoo20191941 <amy0480@naver.com>
2023-06-11 04:05:41 +0900

    :lipstick: Design : 스텝 간격 조정
    design : 스텝 간격 조정

--- src/@components/order/orderStyle/orderStyle.tsx
+++ src/@components/order/orderStyle/orderStyle.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { orderStyleDataType } from "../../../type/order/orderStyleData";
 import SelectedOption from "./selectedOption/selectedOption";
 import Step2 from "./step2/step2";
+import Step3 from "./step3/step3";
 
 export default function OrderStyle() {
   const [orderStyleData, setOrderStyleData] = useState<orderStyleDataType>({
@@ -19,6 +20,7 @@ export default function OrderStyle() {
     <>
       <SelectedOption />
       <Step2 orderStyleData={orderStyleData} setOrderStyleData={setOrderStyleData} />
+      <Step3 orderStyleData={orderStyleData} setOrderStyleData={setOrderStyleData} />
     </>
   );
 }
--- src/@components/order/orderStyle/stepPageLayout/style.tsx
+++ src/@components/order/orderStyle/stepPageLayout/style.tsx
@@ -15,4 +15,6 @@ export const StepWrapper = styled.header`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
+
+  margin-bottom: 80px;
 `;
