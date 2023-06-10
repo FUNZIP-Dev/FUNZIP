218cbd23177a33912e5ba634823a34fe6659a0ea
Seojisoo20191941 <amy0480@naver.com>
2023-06-11 04:02:13 +0900

    :sparkles: Feat : step3 로직 구현
    feat : step3 로직 구현

--- src/@components/order/orderStyle/step3/step3.tsx
+++ src/@components/order/orderStyle/step3/step3.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import StepPageLayout from "../stepPageLayout/stepPageLayout";
 
 export default function Step3() {
-  return <div>step3</div>;
+  return (
+    <StepPageLayout
+      num={3}
+      title="참고할 만한 영상 링크를 업로드해주세요."
+      sub="소비자님들에게 조금 더 취향에 맞는 편집을 해드릴게요">
+      <input type="text" placeholder="링크 복사 후 붙여넣기 해주세요" />
+    </StepPageLayout>
+  );
 }
