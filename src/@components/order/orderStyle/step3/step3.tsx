ea66dabb91c5d7ee0749b27b352cc31731a5d464
Seojisoo20191941 <amy0480@naver.com>
2023-06-11 04:07:53 +0900

    :sparkles: Feat : 링크 입력 로직 구현
    feat : 링크 입력 로직 구현

--- src/@components/order/orderStyle/step3/step3.tsx
+++ src/@components/order/orderStyle/step3/step3.tsx
@@ -5,12 +5,16 @@ import * as S from "./style";
 export default function Step3(props: OrderStyleStepProps) {
   const { orderStyleData, setOrderStyleData } = props;
 
+  const handleInputLink = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setOrderStyleData({ ...orderStyleData, link: e.target.value });
+  };
+
   return (
     <StepPageLayout
       num={3}
       title="참고할 만한 영상 링크를 업로드해주세요."
       sub="소비자님들에게 조금 더 취향에 맞는 편집을 해드릴게요">
-      <S.LinkInput type="text" placeholder="링크 복사 후 붙여넣기 해주세요" />
+      <S.LinkInput type="text" placeholder="링크 복사 후 붙여넣기 해주세요" onChange={() => handleInputLink(e)} />
     </StepPageLayout>
   );
 }
