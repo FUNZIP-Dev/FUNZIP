55a0e47258def1507269aa8823c6185467007a16
Seojisoo20191941 <amy0480@naver.com>
2023-06-11 03:33:31 +0900

    :lipstick: Design : step2 스타일링
    design : step2 스타일링

--- src/@components/order/orderStyle/step2/step2.tsx
+++ src/@components/order/orderStyle/step2/step2.tsx
@@ -1,13 +1,16 @@
 import { MOOD } from "../../../../core/order/styleMood";
+import * as S from "./style";
 
 export default function Step2() {
   return (
     <>
-      <h1>STEP 2.</h1>
-      <h1>영상의 분위기를 선택해주세요.</h1>
-      <p>컷편집 시, 화면 전환의 템포가 달라질 수 있어요!</p>
+      <S.TitleWrapper>
+        <S.Title>STEP 2.</S.Title>
+        <S.Title>영상의 분위기를 선택해주세요.</S.Title>
+        <S.Sub>컷편집 시, 화면 전환의 템포가 달라질 수 있어요!</S.Sub>
+      </S.TitleWrapper>
       {MOOD?.map(({ id, text }) => (
-        <button key={id}>{text}</button>
+        <S.MoodBox key={id}>{text}</S.MoodBox>
       ))}
     </>
   );
---
+++ src/@components/order/orderStyle/step2/style.tsx
@@ -0,0 +1,29 @@
+import { styled } from "styled-components";
+
+export const Title = styled.h1`
+  margin-bottom: 15px;
+
+  ${({ theme }) => theme.fonts.tutorial_head}
+`;
+
+export const Sub = styled.p`
+  color: ${({ theme }) => theme.colors.gray5};
+  ${({ theme }) => theme.fonts.option_content};
+`;
+
+export const TitleWrapper = styled.header`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`;
+
+export const MoodBox = styled.button`
+  width: 200px;
+  height: 56px;
+  margin-right: 17px;
+
+  border: 2px solid ${({ theme }) => theme.colors.gray2};
+  border-radius: 28px;
+
+  background-color: ${({ theme }) => theme.colors.gray1};
+`;
