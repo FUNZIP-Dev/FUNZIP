3fdc13c12716b5d0b3d7c8be3ab435fa380f315a
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2023-06-21 03:37:45 +0900

    :sparkles: feat : 마이페이지 프로세스 완료
    feat : 마이페이지 프로세스 완료

--- src/@pages/myPage/myPage.tsx
+++ src/@pages/myPage/myPage.tsx
@@ -16,6 +16,29 @@ export default function MyPage() {
   const [loading, setLoading] = useState(true); // State variable for loading status
   const [userProfile, setUserProfile] = useState<DocumentData | null>(null); // State variable for user profile
   const [isDoing, setIsDoing] = useState(false); // State variable for loading status
+  const [order, setOrder] = useState(
+    [
+    {
+      category: "브이로그",
+      date: "2021/10/10  23:00",
+      price: "60,000 원",
+      status:0
+    },
+    {
+      category: "코딩",
+      date: "2021/10/10  23:00",
+      price: "100,000 원",
+      status:1
+    },
+    {
+      category: "으각",
+      date: "2021/10/10  23:00",
+      price: "90,000 원",
+      status:2
+    }
+
+  ]
+  )
 
   // Logout function
   const handleLogout = () => {
@@ -80,34 +103,42 @@ export default function MyPage() {
           <S.MypageProcessing isDone={!isDoing}>
           {
               isDoing ? (
-                <S.MypageProcessingContentContainer>
-                  <S.MypageProcessingTitle>
+                <S.MypageProcessingContentContainer isDone={!isDoing}>
+                  <S.MypageProcessingTitle isDone={isDoing}>
                       <S.ProgressText isDone={!isDoing}>편집 진행 중</S.ProgressText>인 내역입니다 :)
                   </S.MypageProcessingTitle>
                   <S.MypageProcessingTitleWrapper>
                     <S.MypageProcessingContentTitle>카테고리</S.MypageProcessingContentTitle>
                     <S.MypageProcessingContentTitle>등록 일시</S.MypageProcessingContentTitle>
                     <S.MypageProcessingContentTitle>결제 정보</S.MypageProcessingContentTitle>
+                    {/* <S.MypageProcessingContentTitle style={{justifyContent:"center"}}>진행 상황</S.MypageProcessingContentTitle> */}
                   </S.MypageProcessingTitleWrapper>
                   <S.MypageProcessingSeparator/>
-                    {/* 밑에 이제 map으로 진행상태 인 것들 나열 해야함 */}
-                  <S.MypageProcessingContentWrapper>
-                    <S.ProcessName>브이로그</S.ProcessName>
-                    <S.ProcessName>2023/01/01  23:00</S.ProcessName>
-                    <S.ProcessName>60,000 원</S.ProcessName>
-                  </S.MypageProcessingContentWrapper>
 
+
+                  {/* 밑에 이제 map으로 진행상태 인 것들 나열 해야함 */}
+                  {order.map((item, index) => (
+                  <S.MypageProcessingContentWrapper key={index}>
+                    <S.ProcessName>{item.category}</S.ProcessName>
+                    <S.ProcessName>{item.date}</S.ProcessName>
+                    <S.ProcessName>{item.price}</S.ProcessName>
+                    <S.ProcessDoing status={item.status}>{item.status === 0 ? "편집 대기" : (item.status === 1 ? "편집 중" : "편집완료")}</S.ProcessDoing>
+                  </S.MypageProcessingContentWrapper>
+                ))}
+
+
+                  
                 </S.MypageProcessingContentContainer>
                 // 진행중이라면
                 
               ) : (
                 // 진행하는게 없다면
                 <>
-                    <S.MypageProcessingTitle>
+                    <S.MypageProcessingTitle isDone={!isDoing}>
                       <S.ProgressText isDone={!isDoing}>편집 진행 중</S.ProgressText>인 내역이 없습니다 :(
                     </S.MypageProcessingTitle>
                 
-                  <S.goOrderBtn>주문하러 가기</S.goOrderBtn>
+                  <S.goOrderBtn isDone={!isDoing}>주문하러 가기</S.goOrderBtn>
                 </>
               )
           }
